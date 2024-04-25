dcbda3716ef39a31a439e9cb5d9184ffb8e35305
mdzihadhasanjaan <mdzihadhasanjaan@gmail.com>
2024-04-25 21:26:04 +0300

    Code added in Program.cs

--- 8.Math/Math.Max/Program.cs
+++ 8.Math/Math.Max/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Type the value of a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Type the value of b:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"The max value is: {Math.Max(a, b)}");
