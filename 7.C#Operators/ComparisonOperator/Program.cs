d52922ef19bc67db4dbd1ed5f40880ea20452d5c
mdzihadhasanjaan <mdzihadhasanjaan@gmail.com>
2024-04-25 18:34:05 +0300

    Code added to program.cs

--- 7.C#Operators/ComparisonOperator/Program.cs
+++ 7.C#Operators/ComparisonOperator/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x = 5;
+int y = 3;
+Console.WriteLine($"Is x grater than y: {x > y}");  // returns true because 5 is greater than 3.
+int a = 5;
+int b = 3;
+Console.WriteLine($"Is a less than b: {a < b}"); // returns false because a is big and b is small.
+Console.WriteLine($"Is x grater or equal to a: {x >= a}"); // returns true because x is equal to a.
+Console.WriteLine($"Is x smaller or equal to b: {x <= b}"); // returns false because x is bigger than and b.
+Console.WriteLine($"Is y is not equal to b: {y != b}"); // returns false because y is equal to b.
