cf8c5cde64cc74d7b53dbb5b9eb75760ac899656
mdzihadhasanjaan <mdzihadhasanjaan@gmail.com>
2024-04-25 18:37:41 +0300

    Code Updated

--- 7.C#Operators/ComparisonOperator/Program.cs
+++ 7.C#Operators/ComparisonOperator/Program.cs
@@ -7,3 +7,4 @@ Console.WriteLine($"Is a less than b: {a < b}"); // returns false because a is b
 Console.WriteLine($"Is x grater or equal to a: {x >= a}"); // returns true because x is equal to a.
 Console.WriteLine($"Is x smaller or equal to b: {x <= b}"); // returns false because x is bigger than and b.
 Console.WriteLine($"Is y is not equal to b: {y != b}"); // returns false because y is equal to b.
+Console.WriteLine($"Is a equal to x: {a == x}"); // returns true because a is equal to x.
