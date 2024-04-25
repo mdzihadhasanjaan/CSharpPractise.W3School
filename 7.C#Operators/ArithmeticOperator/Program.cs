130e70bd46593d84e7f63226d760c9fba72bf6b6
mdzihadhasanjaan <mdzihadhasanjaan@gmail.com>
2024-04-25 17:54:37 +0300

    :

--- 7.C#Operators/ArithmeticOperator/Program.cs
+++ 7.C#Operators/ArithmeticOperator/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1;
+int b = 2;
+int c = 3;
+
+
+int sum = a + b + c;//This is Addition;
+int sub = a - b - c; // THis is substraction;
+int mul = a * b * c; // This is multiplication;
+int mod = a % b; //This is modulus;
+int incre= a++; //This is increment;
+int decre = a--; //This is decrement;
+Console.WriteLine($"This is sum: {sum}"); 
+Console.WriteLine($"This is sub: {sub}");
+Console.WriteLine($"This is mul: {mul}");
+Console.WriteLine($"This is mod: {mod}");
+Console.WriteLine($"This is increment: {incre}");   
+Console.WriteLine($"This is decrement: {decre}");
+
+{
+    
+}
