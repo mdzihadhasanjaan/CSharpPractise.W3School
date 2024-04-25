7823a6581c4f63abc88b58c78831f6bd432efb01
mdzihadhasanjaan <mdzihadhasanjaan@gmail.com>
2024-04-25 18:18:15 +0300

    Code added on program.cs

--- 7.C#Operators/AssignmentOperator/Program.cs
+++ 7.C#Operators/AssignmentOperator/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x = 10;
+x += 7; // This will be x = x + 7, x = 10 + 7 = 17;
+x *= 2;// This will be x = x * 2, x = 17 * 2 = 34;
+x -= 2; // This will be x = x - 2; x = 34 - 2 = 32;
+x /= 2; // This will be x = x / 2; x = 34 / 2 = 16;
+x %= 2; // This will be x = x % 2, X = 16 % 2 = 0;
+x += 10; // This will be again add 10 with the 0, so x = 10;
+x &= 3; // This will be x = x & 2,
+x |= 2; 
+
+Console.WriteLine($"This is the value of x = {x}");
