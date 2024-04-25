d5cef3809ca0ab08dfe7a27f8f7229a5817c40cc
mdzihadhasanjaan <mdzihadhasanjaan@gmail.com>
2024-04-26 00:42:20 +0300

    code added on program.cs

--- 8.Math/Math.Sqrt/Program.cs
+++ 8.Math/Math.Sqrt/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Here I am making a self progrma for square root of any input x
+
+Console.WriteLine($"Enter number and press enter!:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"This is the answer after square root: {Math.Sqrt(number)}.");
+
