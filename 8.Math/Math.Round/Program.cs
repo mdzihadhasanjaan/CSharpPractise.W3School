55d1b61d82459e905f38a2e062f21b533c26b21c
mdzihadhasanjaan <mdzihadhasanjaan@gmail.com>
2024-04-26 10:43:41 +0300

    code added in program.cs

---
+++ 8.Math/Math.Round/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Enter the floating number to get round value: ");
+decimal num = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine($"Here is the round number: {Math.Round(num)}");
